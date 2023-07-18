f0445967d3505a5b04637a69027f1c4199118418
PolarJaba <berttalise@gmail.com>
2023-07-18 08:56:29 +0300

    added fig_data_mart, fixed joins in query

--- dml/data_mart.sql
+++ dml/data_mart.sql
@@ -1,5 +1,4 @@
 WITH
-    -- Создание общей таблицы всех продаж всех магазинов
 	sales_all AS (
 		SELECT product_id, date, sales_cnt,
 		       (SELECT shop_id FROM shops WHERE shop_name = 'DNS') AS shop_id
@@ -10,25 +9,22 @@ WITH
 		UNION DISTINCT SELECT product_id, date, sales_cnt,
 				      (SELECT shop_id FROM shops WHERE shop_name = 'Sitilink') AS shop_id
 			       FROM shop_sitilink),
-	-- Подсчет проданных единиц товара в каждом магазине
-    sales AS (
-		SELECT sold.shop_id, sold.product_id, SUM(sold.sales_cnt) AS sales_fact
-		FROM (SELECT sa.shop_id, sa.product_id, sa.sales_cnt, date, pl.plan_date AS pl_date
+	sales AS (
+		SELECT sold.shop_id, sold.product_id, SUM(sold.sales_cnt) AS sales_fact, sales_plan
+		FROM (SELECT sa.shop_id, sa.product_id, sa.sales_cnt, date, pl.plan_date AS pl_date, pl.plan_cnt AS sales_plan
 		     FROM sales_all sa
 		     INNER JOIN plan pl ON sa.shop_id = pl.shop_id AND sa.product_id = pl.product_id
 		) AS sold
-        -- Фильтрация по конкретному месяцу продаж
 		WHERE date BETWEEN date_trunc('month', pl_date) AND pl_date 
-		GROUP BY shop_id, product_id
+		GROUP BY shop_id, product_id, sales_plan
 		ORDER BY shop_id, product_id
 	)
 SELECT sh.shop_name, pr.product_name, sales_fact,
-	   pl.plan_cnt AS sales_plan, sold.sales_fact/pl.plan_cnt AS "sales_fact/sales_plan",
+	   sold.sales_plan, sold.sales_fact/sold.sales_plan AS "sales_fact/sales_plan",
 	   sold.sales_fact * pr.price AS income_fact,
-	   pl.plan_cnt * pr.price AS income_plan,
-	   (sold.sales_fact * pr.price)/(pl.plan_cnt * pr.price) AS "income_fact/income_plan"
-FROM plan AS pl
-INNER JOIN shops AS sh ON pl.shop_id = sh.shop_id
-INNER JOIN sales AS sold ON pl.shop_id = sold.shop_id AND pl.product_id = sold.product_id
-INNER JOIN products AS pr ON pl.product_id = pr.product_id
-ORDER BY shop_name;
+	   sold.sales_plan * pr.price AS income_plan,
+	   (sold.sales_fact * pr.price)/(sold.sales_plan * pr.price) AS "income_fact/income_plan"
+FROM sales AS sold
+INNER JOIN shops AS sh ON sold.shop_id = sh.shop_id
+INNER JOIN products AS pr ON sold.product_id = pr.product_id
+ORDER BY shop_name, product_name;
